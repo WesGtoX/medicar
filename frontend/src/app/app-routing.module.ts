02347a1a1b514bd17fa0dce59f37d1dcfa480f66
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2020-10-01 23:52:53 -0300

    fix: lib and consultation routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './components/template/login/login.component';
+import { RegisterComponent } from './components/template/register/register.component'
+import { ConsultationListComponent } from './components/template/consultation-list/consultation-list.component';
+import { ConsultationCreateComponent } from './components/template/consultation-create/consultation-create.component'
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: ConsultationListComponent },
+  { path: 'list', component: ConsultationCreateComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { LoginComponent } from './components/template/login/login.component';
+import { RegisterComponent } from './components/template/register/register.component';
+import { ConsultationListComponent } from './components/template/consultation-list/consultation-list.component';
+import { ConsultationCreateComponent } from './components/template/consultation-create/consultation-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ConsultationListComponent,
+    LoginComponent,
+    RegisterComponent,
+    ConsultationCreateComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
