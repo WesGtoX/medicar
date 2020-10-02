551fe9d46a24a6eff3aa7e74c7439b5d3551beca
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2020-10-01 23:30:50 -0300

    feat: consultation create component

---
+++ frontend/src/app/components/template/consultation-create/consultation-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consultation-create',
+  templateUrl: './consultation-create.component.html',
+  styleUrls: ['./consultation-create.component.css']
+})
+export class ConsultationCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
