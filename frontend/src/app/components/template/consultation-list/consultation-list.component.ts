ccdc1e2199f77f522783b6370ab36241408c251a
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2020-10-01 23:30:13 -0300

    feat: consultation list component

---
+++ frontend/src/app/components/template/consultation-list/consultation-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consultation-list',
+  templateUrl: './consultation-list.component.html',
+  styleUrls: ['./consultation-list.component.css']
+})
+export class ConsultationListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
