b03227e7b486c8fcb027155827b4ac531a3b9243
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2020-10-06 06:07:34 -0300

    feat: user login

--- frontend/src/app/components/template/login/login.component.ts
+++ frontend/src/app/components/template/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MainService } from 'src/app/main.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder, 
+    private mainService: MainService,
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      'username': ['', [Validators.required]],
+      'password': ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
+  onSubmit() {
+    let credentials = this.loginForm.value
+    this.mainService.login(credentials).subscribe(() => {
+      this.router.navigate(['home']);
+    }, (err) => {
+      console.error(err);
+    });
+  }
 }
